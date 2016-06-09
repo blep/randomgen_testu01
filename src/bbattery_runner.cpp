ad89c3e4f57fa9ee1124030be677882d80d76e23
Baptiste Lepilleur <baptiste.lepilleur@misys.com>
2016-06-09 11:36:41 +0200

    - Integrated murmur hashing library - Added dumb "counter" RNG and "counter" RNG with murmur hasher applied to count value.

--- src/bbattery_runner.cpp
+++ src/bbattery_runner.cpp
@@ -5,6 +5,9 @@ extern "C" {
 }
 
 #include "pcg_variants.h"
+#include "MurmurHash1.h"
+#include "MurmurHash2.h"
+#include "MurmurHash3.h"
 
 #include <iomanip>
 #include <iostream>
@@ -83,6 +86,56 @@ makeUnif01Gen32( std::string name, FnGetBits getBitsFn, State initialState )
     return std::unique_ptr<Unif01GenBase>( new Unif01Gen32<FnGetBits,State>( name, getBitsFn, initialState ) );
 }
 
+// Dumb increasing counter for "failing" statistical RNG tests
+
+struct CounterState
+{
+    CounterState()
+        : count_( 0 )
+    {
+    }
+
+    uint32_t next()
+    {
+        return count_++;
+    }
+
+    uint32_t count_;
+};
+
+
+std::ostream &operator <<( std::ostream &os, const CounterState &state )
+{
+    os << "count=" << std::setw(8) << std::setfill('0') << state.count_;
+    return os;
+}
+
+uint32_t raw_counter( CounterState *state )
+{
+    return state->next();
+}
+
+uint32_t murmur1_counter( CounterState *state )
+{
+    uint32_t v = state->next();
+    return MurmurHash1Aligned( &v, sizeof(v), 0);
+}
+
+uint32_t murmur2_counter( CounterState *state )
+{
+    uint32_t v = state->next();
+    return MurmurHashAligned2( &v, sizeof(v), 0);
+}
+
+uint32_t murmur3_counter( CounterState *state )
+{
+    uint32_t v = state->next();
+    uint32_t out;
+    MurmurHash3_x86_32( &v, sizeof(v), 0, &out);
+    return out;
+}
+
+
 
 std::unique_ptr<Unif01GenBase> 
 createRng( const std::string &name )
@@ -94,6 +147,14 @@ createRng( const std::string &name )
 
     if ( name == "pcg32_random_r" )
         return makeUnif01Gen32( name, &pcg32_random_r, initState );
+    else if ( name == "raw_counter" )
+        return makeUnif01Gen32( name, &raw_counter, CounterState() );
+    else if ( name == "murmur1_counter" )
+        return makeUnif01Gen32( name, &murmur1_counter, CounterState() );
+    else if ( name == "murmur2_counter" )
+        return makeUnif01Gen32( name, &murmur2_counter, CounterState() );
+    else if ( name == "murmur3_counter" )
+        return makeUnif01Gen32( name, &murmur3_counter, CounterState() );
     else
     {
         printf( "Unknown rng: '%s'. Aborting...", name.c_str() );
